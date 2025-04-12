069ee3209ff2abd1412e7b4b6aec2c30b24af648
VikramNethesh <vikramnethesh@gmail.com>
2025-04-13 00:25:26 +0530

    Updated place of the olympiad with the 'Accepted' version

--- place_of_the_olympiad.cpp
+++ place_of_the_olympiad.cpp
@@ -6,13 +6,16 @@ int main(){
     cin>>test;
     for(long int t=0; t<test; t++){
         cin>>n>>m>>k;
-        for(long int i=1;i<=m;i++){
-            long int avail_cols = (m/(i+1))*i + m%(i+1);
-            if(avail_cols*n>=k){
-                cout<<i<<'\n';
-                break;
-            }
+        long int cols_needed = k/n;
+        if(k%n!=0){
+            cols_needed++;
         }
+        long int free_cols = m-cols_needed;
+        long int max_bench = cols_needed/(free_cols+1);//If there is one free column then it can split the needed columns into 2, if two free cols then 3 parts and so on thats why I have used (free_cols+1)
+        if(cols_needed%(free_cols+1)!=0){
+            max_bench++;
+        }
+        cout<<max_bench<<'\n';
     }
     return 0;
 }
